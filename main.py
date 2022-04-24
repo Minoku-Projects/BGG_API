7ef3bfa75f3ddfff497aa6323c18b61ae48fe7c1
Leon <leon.nussmann@web.de>
2022-04-24 22:54:45 +0200

    move methods to class

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from datetime import datetime
 
 BG_PAGES = "https://boardgamegeek.com/sitemap_geekitems_boardgame_page_"
 
-#list of all single boardgames
-BG_SINGLE_SITES = []
+
 
 
 def mytime():
@@ -14,26 +13,48 @@ def mytime():
     return now.strftime("%H:%M:%S")
     #return now.strftime("%H%M%S%f")
 
-def get_single_sites():
+class robo_data():
 
-    # Max. 15 pages
-    for i in range(15):
-        #range starts with 0 but site starts with 1, so we have to increase
-        number = i + 1
-        #build string for page
-        page = BG_PAGES + str(number)
-        bgg = requests.get(page)
-        print(bgg.status_code)
-        parsed_xml = EleTree.fromstring(bgg.text)
+    # list of all single boardgames
+    bg_sites = []
+    bg_ids = []
 
-        #every url gets extraxted
-        for x in parsed_xml:
-            #x[0].text = https://boardgamegeek.com/boardgame/21776/jot
-            BG_SINGLE_SITES.append(x[0].text)
+    def __init__(self):
+        # Max. 15 pages
+        for i in range(15):
+            #range starts with 0 but site starts with 1, so we have to increase
+            number = i + 1
+            #build string for page
+            page = BG_PAGES + str(number)
+            bgg = requests.get(page)
+            print(bgg.status_code)
+            parsed_xml = EleTree.fromstring(bgg.text)
+
+            #every url gets extraxted
+            for x in parsed_xml:
+                #x[0].text = https://boardgamegeek.com/boardgame/21776/jot
+                self.bg_sites.append(x[0].text)
+
+                #we only want ids and append to our attribute
+                self.__split_url_into_id(x[0].text)
+
+    def get_list_sites(self):
+        return self.bg_sites
+
+    def get_id_list(self):
+        return self.bg_ids
+
+    def __split_url_into_id(self, url):
+        #['https:', '', 'boardgamegeek.com', 'boardgame', '21776', 'jot']
+        #only append ids from url
+        self.bg_ids.append(url.split("/")[4])
+
 
 
 if __name__ == '__main__':
 
-        get_single_sites()
-        print(len(BG_SINGLE_SITES))
+        robo = robo_data()
+        print(len(robo.bg_sites))
+        print(len(robo.bg_ids))
+
 
